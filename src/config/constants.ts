b30797df29462173c6f467cd7ac297151b11d034
Kodai Hagihara <39426616+EringiV3@users.noreply.github.com>
2021-09-02 02:59:25 +0900

    fix

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -2,7 +2,6 @@ export const API_HOST = import.meta.env.VITE_API_HOST?.toString() ?? '';
 export const AUTH0_DOMAIN = import.meta.env.VITE_AUTH0_DOMAIN?.toString() ?? '';
 export const AUTH0_CLIENT_ID =
   import.meta.env.VITE_AUTH0_CLIENT_ID?.toString() ?? '';
-export const AUTH0_REDIRECT_URI =
-  import.meta.env.VITE_AUTH0_REDIRECT_URI?.toString() ?? '';
+export const AUTH0_REDIRECT_URI = window.location.origin;
 export const AUTH0_AUDIENCE =
   import.meta.env.VITE_AUTH0_AUDIENCE?.toString() ?? '';
